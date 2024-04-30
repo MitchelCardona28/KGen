7f5503621d76ae3274648481663b58ea629274ec
MitchelCardona28 <michelito428@gmail.com>
2024-04-29 22:18:38 -0500

    src: moving features out and adding main features

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,5 @@
-import { passwordGenerator } from '../../generators/passGen.js'
-
-const passwordContainer = document.getElementById('passwordContainer')
-// const passwordValue = document.getElementById('passwordValue')
-const generateBtn = document.getElementById('generate')
-const copyBtn = document.getElementById('copy')
-
-const printPassword = () => {
-  const generatedPassword = passwordGenerator(16)
-  const printedPassword = `<p>${generatedPassword}</p>`
-  passwordContainer.innerHTML = printedPassword
-}
-
-const copyPassword = () => {
-  navigator.clipboard.writeText(passwordContainer.children[0].textContent)
-}
+import { generateBtn, copyBtn } from '../../misc/buttons.js'
+import { printPassword, copyPassword } from '../../misc/functions.js'
 
 generateBtn.addEventListener('click', printPassword)
 copyBtn.addEventListener('click', copyPassword)
