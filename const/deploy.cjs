f5029c3c2782e8ee4a4b2db4f68fc71c80bc3770
MitchelCardona28 <michelito428@gmail.com>
2024-05-02 21:13:02 -0500

    upload: no-jekyll option added on publish method

--- const/deploy.cjs
+++ const/deploy.cjs
@@ -2,7 +2,8 @@ const ghpages = require('gh-pages')
 
 const options = {
   branch: 'main',
-  repo: 'https://github.com/MitchelCardona28/KGen.git'
+  repo: 'https://github.com/MitchelCardona28/KGen.git',
+  nojekyll: true
 }
 
 ghpages.publish('../src/', options, (err) => `<p>${err}</p>`)
