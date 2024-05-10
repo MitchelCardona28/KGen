572fcb5797ab03cbf553cbc92ebc3ba79438548b
MitchelCardona28 <michelito428@gmail.com>
2024-05-09 21:23:24 -0500

    update: refactorizing and adding a new parameter to passwordGenerator

--- generators/passGen.js
+++ generators/passGen.js
@@ -1,10 +1,13 @@
 import { CHARS, SPECIALCHARS } from '../const/chars.js'
 import random from './random.js'
 
-export const passwordGenerator = (length) => {
+export const passwordGenerator = (length, excludeSpecialChars) => {
+  const passwordChars = excludeSpecialChars ? CHARS.replace(SPECIALCHARS, '') : CHARS
+
   const generatedPassword = Array.from(Array(length), () => {
-    const randomChars = CHARS.charAt(random(CHARS.length))
-    return randomChars
+    const randomIndex = random(CHARS.length)
+    const randomChar = passwordChars.charAt(randomIndex)
+    return randomChar
   }).join('')
 
   return generatedPassword
